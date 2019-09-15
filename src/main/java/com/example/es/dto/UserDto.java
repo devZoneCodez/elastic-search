07da1e6f9e81eb28b919b85b483c09f15ed8c145
Sateesh Kumar D <sateeshkumar.d@gmail.com>
2019-09-15 23:42:58 +0530

    No argument constructor has been added to UserDto.

--- src/main/java/com/example/es/dto/UserDto.java
+++ src/main/java/com/example/es/dto/UserDto.java
@@ -1,10 +1,10 @@
 package com.example.es.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
+@NoArgsConstructor
 public class UserDto {
     private Long id;
     private String firstName;
