ac7313645682c44a13f3a4efc9f6f6c112fffe76
Sateesh Kumar D <sateeshkumar.d@gmail.com>
2019-09-15 23:42:09 +0530

    Basic UserController has been created. No argument constructor has been added to UserDto.

---
+++ src/main/java/com/example/es/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.example.es.controller;
+
+import com.example.es.dto.UserDto;
+import com.example.es.model.User;
+import com.example.es.service.UserService;
+import com.example.es.util.Converter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private Converter converter;
+
+    @PostMapping(path = "add"
+            , consumes = MediaType.APPLICATION_JSON_VALUE
+            , produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public UserDto addUser(@RequestBody UserDto userDto) {
+        User user = converter.convertToEntity(userDto);
+        user.setFirstName("Name has been edited in the controller");
+        UserDto tempDtp = converter.convertToDto(user);
+        return tempDtp;
+    }
+
+}
