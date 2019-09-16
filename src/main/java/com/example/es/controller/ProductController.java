1cd3aa1be2323851c35aa9487a0097efdaa070a5
Sateesh Kumar D <sateeshkumar.d@gmail.com>
2019-09-16 23:01:46 +0530

    Added ProductController to provide the CRUD operations.

--- src/main/java/com/example/es/controller/ProductController.java
+++ src/main/java/com/example/es/controller/ProductController.java
@@ -1,17 +1,12 @@
 package com.example.es.controller;
 
 import com.example.es.dto.ProductDto;
-import com.example.es.dto.UserDto;
-import com.example.es.model.User;
 import com.example.es.service.ProductService;
-import com.example.es.service.UserService;
-import com.example.es.util.Converter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("product")
@@ -20,14 +15,58 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    @GetMapping(path = "all"
+            , produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<ProductDto> findAllProducts() {
+        return productService.findAll();
+    }
+
+    @GetMapping(path = "find/{productId}"
+            , produces = MediaType.APPLICATION_JSON_VALUE)
+    public ProductDto find(@PathVariable(name = "productId") Long productId) {
+        return productService.findById(productId);
+    }
+
     @PostMapping(path = "add"
             , consumes = MediaType.APPLICATION_JSON_VALUE
             , produces = MediaType.APPLICATION_JSON_VALUE
     )
     public ProductDto addProduct(@RequestBody ProductDto productDto) {
-        //return productService.save(productDto)
-        productDto.setProductDesc("Description has been edited!!!");
-        return productDto;
+        return productService.save(productDto);
+        //productDto.setProductDesc("Description has been edited!!!");
+        //return productDto;
+    }
+
+    @PostMapping(path = "addAll"
+            , consumes = MediaType.APPLICATION_JSON_VALUE
+            , produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public List<ProductDto> addProducts(@RequestBody List<ProductDto> products) {
+        return productService.saveAll(products);
+        //return products;
+    }
+
+    @PutMapping(path = "update/{productId}"
+            , consumes = MediaType.APPLICATION_JSON_VALUE
+            , produces = MediaType.APPLICATION_JSON_VALUE)
+    public ProductDto updateProduct(@RequestBody ProductDto productDto
+            , @PathVariable(name = "productId") Long productId) {
+        ProductDto p = productService.findById(productId);
+        ProductDto savedProductDto = null;
+        if (p != null) {
+            savedProductDto = productService.save(productDto);
+        }
+        return savedProductDto;
+    }
+
+    @DeleteMapping("delete/{productId}")
+    public ProductDto deleteProduct(@PathVariable(name = "productId") Long productId) {
+        ProductDto p = productService.findById(productId);
+        ProductDto deletedProductDto = null;
+        if (p != null) {
+            deletedProductDto = productService.delete(p);
+        }
+        return deletedProductDto;
     }
 
 }
