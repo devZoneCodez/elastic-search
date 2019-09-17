41c0d7150af2e6c41fc90ee8628748dc636a4aaf
Sateesh Kumar D <sateeshkumar.d@gmail.com>
2019-09-17 21:33:41 +0530

    Models and respective DTO(s) are made serializable.

--- src/main/java/com/example/es/dto/UserDto.java
+++ src/main/java/com/example/es/dto/UserDto.java
@@ -3,9 +3,11 @@ package com.example.es.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @NoArgsConstructor
-public class UserDto {
+public class UserDto implements Serializable {
     private Long id;
     private String firstName;
     private String lastName;
